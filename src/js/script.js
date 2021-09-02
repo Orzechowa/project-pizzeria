6844f330f3a3ffc3ceb2f6120eccc462eff8ced3
Ewa Orzechowska <orzechowska1989@wp.pl>
2021-09-03 00:46:56 +0200

    add image

--- src/js/script.js
+++ src/js/script.js
@@ -92,6 +92,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -160,9 +161,9 @@
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           console.log(optionId, option);
-
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)) {
+          if(optionSelected) {
             // check if the option is not default
             if(!option.default == true) {
             // add option price to price variable
@@ -175,13 +176,26 @@
               price -= option.price;
             }
           }
+
+            // found image with class paramId-optionId , check is it found ,if it is show img if not hide it
+            const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+
+            if(optionImage){
+              if(optionSelected){
+                optionImage.classList.add(classNames.menuProduct.imageVisible);
+              }else {
+                optionImage.classList.remove(classNames.menuProduct.imageVisible);
+              }
+
+            }
+          }
         }
 
         // update calculated price in the HTML
         thisProduct.priceElem.innerHTML = price;
       }
     }
-  }
+  
   const app = {
     initMenu: function(){
       const thisApp = this;
